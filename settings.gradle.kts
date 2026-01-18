c0c4cbfea443c0a2674bc4f890d9d270d3b5a7cf
SallyLueNoa <sally.lue.noa@gmail.com>
2026-01-18 13:55:45 +0900

    [Feature implementation] Create new project. (#1)
    Create the Ktor project by using IntelliJ.

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "FRLineAgent"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ src/test/kotlin/ApplicationTest.kt
@@ -0,0 +1,21 @@
+package org.fog_rock
+
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ApplicationTest {
+
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            module()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+        }
+    }
+
+}
