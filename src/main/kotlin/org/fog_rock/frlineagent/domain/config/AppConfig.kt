0c752af4ed5a843cfe48605b674487b39d76b65d
SallyLueNoa <sally.lue.noa@gmail.com>
2026-02-03 23:54:47 +0900

    [Feature implementation] Implement core APIs. (Infrastracture Layer) (#7)
    * Introduce AppConfig abstraction for external service providers
      * Add domain.AppConfig to define provider mode enum.
      * Add infrastructure.KtorAppConfig for YAML-based configuration.

---
+++ src/main/kotlin/org/fog_rock/frlineagent/domain/config/AppConfig.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2026 SallyLueNoa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.fog_rock.frlineagent.domain.config
+
+/**
+ * An interface for managing the integration mode with external services.
+ */
+interface AppConfig {
+    /**
+     * Provider mode.
+     */
+    enum class ProviderMode {
+        /** Use cloud services. */
+        CLOUD,
+        /** Use mocks. */
+        MOCK,
+    }
+
+    /** Mode for Secret Manager. */
+    val secretManagerMode: ProviderMode
+    /** Mode for Spreadsheet. */
+    val spreadsheetMode: ProviderMode
+    /** Mode for LINE API. */
+    val lineApiMode: ProviderMode
+}
