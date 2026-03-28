10a8b2a619cde7b13f0b394483de0d844f5b1474
SallyLueNoa <sally.lue.noa@gmail.com>
2026-03-28 23:42:03 +0900

    [Feature implementation] Implement Ktor Plugin to the core library. (#34)
    * Add the library `ktor-server-core` so that the plugin API can be used.

--- fr-line-agent-core/build.gradle.kts
+++ fr-line-agent-core/build.gradle.kts
@@ -24,6 +24,7 @@ kotlin {
 }
 
 dependencies {
+    api(libs.ktor.server.core)
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.logback.classic)
